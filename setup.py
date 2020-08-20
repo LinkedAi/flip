1aa2979b8db06e30e3ce5345e6597ce6d1bd4169
Diego Parra <diego@linkedai.co>
2020-08-19 20:44:59 -0500

    Update setup.py to upload to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup
 
 setup(name='flip',
-      version='0.1',
-      description='Synthetic Data',
+      version='0.1.0',
+      description='Generate thousands of new 2D images from a small batch of objects and backgrounds.',
       pakages=['flip'],
+      license='GPL-3.0',
+      author='Linked AI Inc',
+      author_email='d@linkedai.co',
       zip_safe=False)
