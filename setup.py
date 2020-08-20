75240203df5c6b6966b5027956da034baf59e0e0
Diego Parra <diego@linkedai.co>
2020-08-19 22:49:36 -0500

    Add extra data to the setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as req_file:
+    requirements = req_file.read().splitlines()
+
 setup(name='flip',
       version='0.1.0',
-      description='Generate thousands of new 2D images from a small batch of objects and backgrounds.',
-      pakages=['flip'],
       license='GPL-3.0',
       author='Linked AI Inc',
       author_email='d@linkedai.co',
-      zip_safe=False)
+      description='Generate thousands of new 2D images from a small batch of objects and backgrounds.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='',
+      packages=['flip'],
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+      ],
+      install_requires=requirements,
+      python_requires='>=3.7')
