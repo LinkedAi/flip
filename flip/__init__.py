9b8900280b8b6732087d38ab341ef7815e58c0a6
Diego Parra <diego@linkedai.co>
2020-08-22 22:08:57 -0500

    Add the script to check the label

--- flip/__init__.py
+++ flip/__init__.py
@@ -1,12 +1,10 @@
 """
 flip
 """
+from . import transformers, utils
 
 about = {}
-with open("flip/__about__.py") as fp:
+with open('flip/__about__.py') as fp:
     exec(fp.read(), about)
 
 __version__ = about['__version__']
-
-from . import transformers
-from . import utils
