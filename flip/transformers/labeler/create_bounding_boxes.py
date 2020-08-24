3b890117c7d7ac592e7d46444aaf1bc7372061c1
Diego Parra <diego@linkedai.co>
2020-08-24 11:36:47 -0500

    Update the way bounding boxes are created to fix error of last contour, and use all contours instead

--- flip/transformers/labeler/create_bounding_boxes.py
+++ flip/transformers/labeler/create_bounding_boxes.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 from flip.transformers.element import Element
 from flip.transformers.transformer import Transformer
@@ -36,29 +38,36 @@ class CreateBoundingBoxes(Transformer):
         """
 
         # Canny edge detection - edge gradient
-        edged = cv2.Canny(image, 10, 250)
+        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_filtered = cv2.bilateralFilter(img_gray, 7, 50, 50)
+        edged = cv2.Canny(gray_filtered, 60, 120)
+        edges_filtered = cv2.Canny(edged, 60, 120)
 
         # Morphological Transformations
         # applying closing function
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-        closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+        closed = cv2.morphologyEx(edges_filtered, cv2.MORPH_CLOSE, kernel)
 
         # Finding_contours
         (cnts, _) = cv2.findContours(
             closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
         )
 
+        x1, x2 = img_gray.shape[1], 0
+        y1, y2 = img_gray.shape[0], 0
         for c in cnts:
-            peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
-        global x, y, w, h
-        global new_img
-
-        for c in cnts:
             x, y, w, h = cv2.boundingRect(c)
 
-            if w > 50 and h > 50:
-                new_img = image[y: y + h, x: x + w]
+            if x < x1:
+                x1 = x
+            if y < y1:
+                y1 = y
+            if x+w > x2:
+                x2 = x+w
+            if y+h > y2:
+                y2 = y+h
 
-        return x, y, new_img.shape[1], new_img.shape[0]
+        # new_img = image[y1: y2, x1: x2]
+
+        return x1, y1, x2, y2
