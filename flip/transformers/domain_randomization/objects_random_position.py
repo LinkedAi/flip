06e771a61262d655dfbff602f77418cb7f25ad1e
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2021-08-26 15:51:40 -0500

    Update objects_random_position.py

--- flip/transformers/domain_randomization/objects_random_position.py
+++ flip/transformers/domain_randomization/objects_random_position.py
@@ -1,4 +1,5 @@
 import numpy as np
+import warnings
 
 from flip.transformers.element import Element
 from flip.transformers.transformer import Transformer
@@ -11,9 +12,9 @@ class ObjectsRandomPosition(Transformer):
         Parameters
         ----------
         mode : {'random', 'percentage'}, default='random'
-        overlap : percentage 0 to 1, default=0
-                  1 for allowing a complete overlap
-                  0 for avoiding overlap
+        force_overlap : if True allows overlap
+                        if it is false it does not allow the overlap
+                        default = True
     """
     _SUPPORTED_MODES = {'random', 'percentage'}
 
@@ -24,20 +25,20 @@ class ObjectsRandomPosition(Transformer):
         x_max=None,
         y_min=None,
         y_max=None,
-        overlap=0
+        force_overlap=True
     ):
         self.x_min = x_min
         self.x_max = x_max
         self.y_min = y_min
         self.y_max = y_max
-        self.overlap = overlap
         self.mode = mode
+        self.force_overlap = force_overlap
 
         if self.mode not in self._SUPPORTED_MODES:
             raise ValueError("Mode '{0:s}' not supported. ".format(self.mode))
 
     def makemask(self, masks, obj_h, obj_w, low_x, high_x, low_y, high_y,
-                 el_h, el_w, factor):
+                 el_h, el_w, factor, force_overlap):
         """ Set a mask
 
             Parameters
@@ -66,18 +67,31 @@ class ObjectsRandomPosition(Transformer):
 
         # iterate masks - construct and add new masks
         for m in masks:
-            c_x = m['w'] / 2
-            c_y = m['h'] / 2
+            if force_overlap:
+                c_x = m['w'] / 2
+                c_y = m['h'] / 2
 
-            y1 = int(m['y'] + (1 - factor) * c_y)
-            x1 = int(m['x'] + (1 - factor) * c_x)
-            y2 = int(m['y'] + m['h'] - (1 - factor) * c_y)
-            x2 = int(m['x'] + m['w'] - (1 - factor) * c_x)
+                y1 = int(m['y'] + (1 - factor) * c_y)
+                x1 = int(m['x'] + (1 - factor) * c_x)
+                y2 = int(m['y'] + m['h'] - (1 - factor) * c_y)
+                x2 = int(m['x'] + m['w'] - (1 - factor) * c_x)
 
-            y_a = max(0, y1 - obj_h)
-            x_a = max(0, x1 - obj_w)
-            grid[y_a:y2, x_a:x2] = 0
+                y_a = max(0, y1 - obj_h)
+                x_a = max(0, x1 - obj_w)
+                y_b = min(el_h, y2)
+                x_b = min(el_w, x2)
+                grid[y_a:y_b, x_a:x_b] = 0
+            else:
+                y1 = int(m['y'])
+                x1 = int(m['x'])
+                y2 = int(m['y'] + m['h'])
+                x2 = int(m['x'] + m['w'])
 
+                y_a = max(0, y1 - obj_h)
+                x_a = max(0, x1 - obj_w)
+                y_b = min(el_h-5, y2)
+                x_b = min(el_w-5, x2)
+                grid[y_a:y_b, x_a:x_b] = 0
         return grid
 
     def map(self, element: Element) -> Element:
@@ -113,9 +127,11 @@ class ObjectsRandomPosition(Transformer):
             x_max = self.x_max if self.x_max is not None else el_w
             y_min = self.y_min if self.y_min is not None else 0
             y_max = self.y_max if self.y_max is not None else el_h
-
+            
+        delete=[]
         masks = []
-        for obj in element.objects:
+        for f,obj in enumerate(element.objects):
+            
             # object dimension
 
             obj_h: int = int(obj.image.shape[0])
@@ -123,47 +139,54 @@ class ObjectsRandomPosition(Transformer):
 
             # max and min limits for the object with respect to the background
             low_x = int(max(0, x_min))
-            high_x = int(min(x_max, el_w - obj_w))
+            high_x = int(min(x_max, el_w))
             low_y = int(max(0, y_min))
-            high_y = int(min(y_max, el_h - obj_h))
+            high_y = int(min(y_max, el_h))
 
             # overlapping percentage
-            factor = self.overlap
+            factor = 0
 
             # index_av stores available positions to place the object
             index_av = []
-
-            # find positions to place the object
-            # if there is not space the overlapping is increased
-            while len(index_av) == 0 and factor < 1:
-                factor += 0.1
-                params = [masks, obj_h, obj_w, low_x,
-                          high_x, low_y, high_y, el_h, el_w, factor]
-                grid_available = self.makemask(*params)
-                index_av = np.argwhere(grid_available)
-
-            # if despite increasing overlapping there is not space
-            # the limits are moved based on a new factor
-            factor = 0
-            while len(index_av) == 0:
-                if high_y == (el_h - obj_h) and high_x == (el_w - obj_w):
-                    # when the object doesn't have an available space
-                    # despite changing overlapping and limits
-                    # the complete background is available
-                    index_av = np.argwhere(np.ones((el_h, el_w)))
-                low_x = int(max(0, x_min - factor))
-                high_x = int(min(x_max + factor, (el_w - obj_w)))
-                low_y = int(max(0, y_min - factor))
-                high_y = int(min(y_max + factor, el_h - obj_h))
-                grid_available = np.zeros((el_h, el_w))
-                grid_available[low_y:high_y, low_x:high_x] = 1
-                index_av = np.argwhere(grid_available)
-                factor += 1
-
-            # random index
+            if self.force_overlap==True:
+                # find positions to place the object
+                # if there is not space the overlapping is increased
+                while len(index_av) == 0:
+                    factor += 0.1
+                    params = [masks, obj_h, obj_w, low_x,
+                              high_x, low_y, high_y, el_h, el_w, factor, 
+                              self.force_overlap]
+                    grid_available = self.makemask(*params)
+                    index_av = np.argwhere(grid_available)
+                    if factor>1 and len(index_av)==0:
+                        factor=9999
+                        break
+            else:
+                cont=0
+                while len(index_av) == 0:
+                    params = [masks, obj_h, obj_w, low_x,
+                              high_x, low_y, high_y, el_h, el_w, factor,
+                              self.force_overlap]
+                    grid_available = self.makemask(*params)
+                    index_av = np.argwhere(grid_available)
+                    cont+=1
+                    if cont==10 and len(index_av)==0:
+                        factor=9999
+                        break
+                    
+            if factor==9999:                
+                delete.append(f)
+                continue
+            
             index = np.random.choice(index_av.shape[0])
             obj.y, obj.x = int(index_av[index][0]), int(index_av[index][1])
             # save mask for the inserted object
             masks.append({'x': obj.x, 'y': obj.y, 'w': obj_w, 'h': obj_h})
-
+        a=0
+        for s in delete:
+            element.objects.pop(s-a)
+            a+=1
+        if len(delete)!=0:
+            warnings.warn('There is no space available to place all the objects')
+        
         return element
