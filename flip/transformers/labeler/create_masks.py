60409bbe52139282a9443bce28bf348d3f00edd0
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2021-09-09 16:08:45 -0500

    Update create_masks.py

--- flip/transformers/labeler/create_masks.py
+++ flip/transformers/labeler/create_masks.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cv2
 
 from flip.transformers.element import Element
 from flip.transformers.transformer import Transformer
@@ -9,7 +8,7 @@ class CreateMasks(Transformer):
     def __init__(self, classes=[]):
         self.classes = classes
     
-    def map(self, element: Element) -> Element:
+    def map(self, element: Element, parent=None) -> Element:
         assert element, "element cannot be None"
 
         element.masks = self.create(element)
@@ -17,7 +16,8 @@ class CreateMasks(Transformer):
         return element
 
     def create(self, element):
-        masks = np.zeros((element.created_image.shape[0],element.created_image.shape[1]))
+        mask = np.zeros((element.created_image.image.shape[0],element.created_image.image.shape[1],3))
+        masks = np.zeros((element.created_image.image.shape[0],element.created_image.image.shape[1]))
         classes = self.classes
         
         for i, obj in enumerate(element.objects):
@@ -26,7 +26,7 @@ class CreateMasks(Transformer):
             y = obj.y
             w = obj.image.shape[1]
             h = obj.image.shape[0]
-            if x>=element.created_image.shape[1] or y>=element.created_image.shape[0]:
+            if x>=element.created_image.image.shape[1] or y>=element.created_image.image.shape[0]:
                 continue
             if y+h>masks.shape[0]:
                 h = masks.shape[0]-y
@@ -36,7 +36,7 @@ class CreateMasks(Transformer):
                 obj.image = obj.image[:,:w,:]
             
             # Create the first mask
-            obje = cv2.cvtColor(obj.image, cv2.COLOR_BGR2GRAY)
+            obje = obj.image[:,:,3]
             if i == 0:
                 masks[y:y+h,x:x+w]+=np.logical_or(masks[y:y+h,x:x+w],obje)*(classes.index(obj.name)+1)
                 continue
@@ -49,4 +49,12 @@ class CreateMasks(Transformer):
                         masks[y+a,x+b] = 0
             # Place the final object mask
             masks[y:y+h,x:x+w]+=np.logical_or(masks[y:y+h,x:x+w]*0,obje)*(classes.index(obj.name)+1)
-        return masks
+        
+        if type(element.created_image.cut_out) != type(None):
+            coords = np.argwhere(element.created_image.cut_out.image)
+            for i in coords:
+                masks[i[0],i[1]]=0
+        mask[:,:,0] = (masks+1)*255/len(classes)
+        mask[:,:,1] = (masks+1)*255/len(classes)
+        mask[:,:,2] = (masks+1)*255/len(classes)
+        return mask
