5388cc376819ccf730b71a0ca938c0d896e788df
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2021-09-09 15:54:38 -0500

    Update __init__.py

--- flip/transformers/data_augmentation/__init__.py
+++ flip/transformers/data_augmentation/__init__.py
@@ -2,3 +2,5 @@ from .flip import Flip
 from .rotate import Rotate
 from .random_resize import RandomResize
 from .color import Color, Brightness, Contrast, Saturation
+from .noise import Noise
+from .cut_out import CutOut
