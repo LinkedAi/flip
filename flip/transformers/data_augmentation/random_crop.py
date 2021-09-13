ca18c83016d22971b519d6ac34be7cb75af428c9
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2021-09-13 15:57:04 -0500

    Update random_crop.py

--- flip/transformers/data_augmentation/random_crop.py
+++ flip/transformers/data_augmentation/random_crop.py
@@ -58,36 +58,36 @@ class RandomCrop(Transformer):
                     w = np.random.randint(x_min,el_w)
                     
                     element.image = element.image[y:min(y+h,el_h),x:min(x+w,el_w),:]
+        else:
+            if self.mode == 'random':
+        
+                x = np.random.randint(0,el_w-el_w/5)
+                y = np.random.randint(0,el_h-el_h/5)
+                
+                h = np.random.randint(el_h/5,el_h)
+                w = np.random.randint(el_w/5,el_w)
+                
+                element.image = element.image[y:min(y+h,el_h),x:min(x+w,el_w),:]
+                
             else:
-                if self.mode == 'random':
-            
-                    x = np.random.randint(0,el_w-el_w/5)
-                    y = np.random.randint(0,el_h-el_h/5)
-                    
-                    h = np.random.randint(el_h/5,el_h)
-                    w = np.random.randint(el_w/5,el_w)
-                    
-                    element.image = element.image[y:min(y+h,el_h),x:min(x+w,el_w),:]
-                    
-                else:
-                    
-                    x_min = (
-                        (self.x_min if self.x_min <= 1 else (self.x_min % 100) / 100) * el_w
-                        if self.x_min is not None
-                        else 0
-                    )
-                    y_min = (
-                        (self.y_min if self.y_min <= 1 else (self.y_min % 100) / 100) * el_h
-                        if self.y_min is not None
-                        else 0
-                    )
-                    
-                    x = np.random.randint(0,el_w-x_min)
-                    y = np.random.randint(0,el_h-y_min)
-                    
-                    h = np.random.randint(y_min,el_h)
-                    w = np.random.randint(x_min,el_w)
-                    
-                    element.image = element.image[y:min(y+h,el_h),x:min(x+w,el_w),:]
+                
+                x_min = (
+                    (self.x_min if self.x_min <= 1 else (self.x_min % 100) / 100) * el_w
+                    if self.x_min is not None
+                    else 0
+                )
+                y_min = (
+                    (self.y_min if self.y_min <= 1 else (self.y_min % 100) / 100) * el_h
+                    if self.y_min is not None
+                    else 0
+                )
+                
+                x = np.random.randint(0,el_w-x_min)
+                y = np.random.randint(0,el_h-y_min)
+                
+                h = np.random.randint(y_min,el_h)
+                w = np.random.randint(x_min,el_w)
+                
+                element.image = element.image[y:min(y+h,el_h),x:min(x+w,el_w),:]
             
         return element
