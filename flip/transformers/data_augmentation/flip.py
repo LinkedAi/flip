04499a887911972fbcd7cc95f919314d63e3c511
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2021-09-13 09:29:55 -0500

    Update flip.py

--- flip/transformers/data_augmentation/flip.py
+++ flip/transformers/data_augmentation/flip.py
@@ -17,14 +17,19 @@ class Flip(Transformer):
     """
     _SUPPORTED_MODES = {'random', 'x', 'y'}
 
-    def __init__(self, mode='random'):
+    def __init__(self, mode='random', force=True):
         self.mode = mode
+        self.force = force
 
         if self.mode not in self._SUPPORTED_MODES:
             raise ValueError("Mode '{0:s}' not supported. ".format(self.mode))
 
     def map(self, element: Element, parent=None) -> Element:
         assert element, "Element cannot be None"
+        
+        if self.force == False:
+            if np.random.randint(low=0, high=2) == 0:
+                self.force = True
 
         if self.mode == 'x':
             direction = 0
@@ -33,7 +38,7 @@ class Flip(Transformer):
         else:
             direction = np.random.randint(low=0, high=2)
 
-        if np.random.randint(low=0, high=2) == 0:  # Flip if 0
+        if self.force == True:
             element.image = cv2.flip(element.image, direction)
 
         return element
