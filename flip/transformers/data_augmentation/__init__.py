932bb7382beb5b402c23ea4fbcf3264e7c8cc08c
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2021-09-30 16:18:59 -0500

    Create cut out feature (#36)
    * Update __init__.py
    
    * Create cut_out.py
    
    * Update README.md
    
    * Update cut_out.py
    
    * Update cut_out.py
    
    Co-authored-by: Diego Parra <diego@linkedai.co>

--- flip/transformers/data_augmentation/__init__.py
+++ flip/transformers/data_augmentation/__init__.py
@@ -3,3 +3,5 @@ from .rotate import Rotate
 from .random_resize import RandomResize
 from .color import Color, Brightness, Contrast, Saturation
 from .noise import Noise
+from .cut_out import CutOut
+
