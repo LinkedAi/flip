1e10ecd5daaa13828fa8e16c16506c721834ecae
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2021-10-28 13:37:10 -0500

    Create ApplyTo options (#38)
    * Update __init__.py
    
    * Update transformer.py

--- flip/transformers/transformer.py
+++ flip/transformers/transformer.py
@@ -1,8 +1,16 @@
 import typing as tp
+import warnings
 
+import flip
 from flip import parser
 from flip.transformers.element import Element
 
+warnings.simplefilter('always', UserWarning)
+def custom_formatwarning(msg, *args, **kwargs):
+    # ignore everything except the message
+    return str(msg) + '\n'
+
+warnings.formatwarning = custom_formatwarning
 
 class Transformer:
     @classmethod
@@ -74,5 +82,40 @@ class ApplyToObjects(Transformer):
     def map(self, element: Element) -> Element:
         for obj in element.objects:
             for system in self.transforms:
+                if type(system) == flip.transformers.domain_randomization.Draw or type(system) == flip.transformers.domain_randomization.ObjectsRandomPosition:
+                    warnings.warn('{}. This transformer is not allowed for the object image.'.format(type(system)))
+                    continue
                 obj = system.map(obj, parent=element)
+        return element
+
+@parser.parseable
+class ApplyToBackground(Transformer):
+    def __init__(self, transforms: tp.List[Transformer]):
+        if hasattr(transforms, "__iter__"):
+            self.transforms = transforms
+        else:
+            self.transforms = [transforms]
+
+    def map(self, element: Element) -> Element:
+        for system in self.transforms:
+            if type(system) == flip.transformers.domain_randomization.Draw or type(system) == flip.transformers.domain_randomization.ObjectsRandomPosition:
+               warnings.warn(f'{type(system)}. This transformer is not allowed for the background image.')
+               continue
+            element = system.map(element, parent=element)
+        return element
+    
+@parser.parseable
+class ApplyToCreatedImage(Transformer):
+    def __init__(self, transforms: tp.List[Transformer]):
+        if hasattr(transforms, "__iter__"):
+            self.transforms = transforms
+        else:
+            self.transforms = [transforms]
+
+    def map(self, element: Element) -> Element:
+        for system in self.transforms:
+            if type(system) == flip.transformers.data_augmentation.Rotate or type(system) == flip.transformers.domain_randomization.Draw or type(system) == flip.transformers.domain_randomization.ObjectsRandomPosition or type(system) == flip.transformers.data_augmentation.RandomCrop or type(system) == flip.transformers.data_augmentation.Flip or type(system) == flip.transformers.data_augmentation.RandomResize:
+               warnings.warn(str(type(system))+' This transformer is not allowed for the created image.')
+               continue
+            element.created_image = system.map(element.created_image, parent=element)
         return element
