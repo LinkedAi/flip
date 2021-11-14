524bb31d958b1f28761f97beeec58824f74b70f9
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2021-11-13 22:23:28 -0500

    IO update (#39)
    * Update pascal_voc.py
    
    * Update json.py
    
    * Update csv.py
    
    * Update rotate.py
    
    * Correct comments

--- flip/transformers/io/json.py
+++ flip/transformers/io/json.py
@@ -1,5 +1,7 @@
 import os
 import json
+import warnings
+warnings.simplefilter('always', UserWarning)
 
 from flip.transformers.element import Element
 from flip.transformers.transformer import Transformer
@@ -12,12 +14,13 @@ class CreateJson(Transformer):
 
     def map(self, element: Element) -> Element:
         assert element, "element cannot be None"
-
         json_path = os.path.join(self.out_dir, f"{self.name}.json")
-        json_data = json.dumps(element.tags)
-
-        with open(json_path, mode="w") as f:
-            f.write(json_data)
-
+        if type(element.tags) != type(None):
+            json_data=json.dumps(element.tags)
+            with open(json_path, mode="w") as f:
+                f.write(json_data)
+        else:
+            warnings.warn('The JSON file has not been created since the bounding boxes do not exist')
+        
         return element
 
