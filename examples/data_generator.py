1db90c572e7a1b0f5797ec8e8f76480afc6cbb9e
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2021-11-17 15:56:03 -0500

    Documentation update (#43)
    * Update data_generator.py
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- examples/data_generator.py
+++ examples/data_generator.py
@@ -2,7 +2,8 @@
 """
 Code Information:
 	Developers:  Diego Parra     - LinkedAi  - diego@linkedai.co
-              Juan Jurado(JJ) - LinkedAi  - jj@linkedai.co
+                 Juan Jurado(JJ) - LinkedAi  - jj@linkedai.co
+                 Juan Montenegro - LinkedAi  - juan@linkedai.co
 Description:
   - TODO: Some description about this .py file
 Tested on:
@@ -29,7 +30,7 @@ from glob import glob
 # Environment global variables
 # ==============================================================================
 # Number of output images when the data_generator.py is ejecuted
-N_SAMPLES = 200
+N_SAMPLES = 10
 
 # Data - path for background images
 BACKGROUNDS_PATTERN = "examples/data/backgrounds/*"
@@ -44,11 +45,14 @@ DATE = datetime.now().strftime("%Y%m%d_%H%M%S")
 # create name for result folder dir
 OUT_DIR = "examples/result/{}".format(DATE)
 
+#Create classes array
+CLASSES_PATTERN ="examples/data/objects/**"
+
 # Object randomization
 # number of objects for full image
 # (1) = only one image -
 # (1,3) = random objects number
-N_OBJECTS = (1, 5)
+N_OBJECTS = (1,4)
 
 
 # ==============================================================================
@@ -65,7 +69,7 @@ def create_out_dir():
     os.makedirs(OUT_DIR, exist_ok=True)
 
 
-def setup_environment(objects_pattern, backgrounds_pattern, n_samples):
+def setup_environment(objects_pattern, backgrounds_pattern, n_samples, classes):
     """
   Args:
     objects_pattern:      path for objects images
@@ -80,17 +84,19 @@ def setup_environment(objects_pattern, backgrounds_pattern, n_samples):
     objects_paths = glob(objects_pattern)
     # get background paths
     backgrounds_paths = glob(backgrounds_pattern)
+    
+    classes_names = [os.path.basename(i) for i in glob(classes)]
 
     elements = []
 
     for _ in range(n_samples):
-        el = create_element(objects_paths, backgrounds_paths)
+        el = create_element(objects_paths, backgrounds_paths, classes_names)
         elements.append(el)
 
     create_google_csv(elements)
 
 
-def create_element(objects_paths, backgrounds_paths):
+def create_element(objects_paths, backgrounds_paths, classes_names):
     """
   Args:
   Description:
@@ -107,6 +113,7 @@ def create_element(objects_paths, backgrounds_paths):
 
     # Create elements for the object images
     objects = [create_child(i) for i in object_idxs]
+    
 
     # get random background
     background_idx = np.random.randint(len(backgrounds_paths))
@@ -115,12 +122,12 @@ def create_element(objects_paths, backgrounds_paths):
     )
 
     # create new element
-    el = flip.transformers.Element(image=background_image, objects=objects)
+    el = flip.transformers.Element(image=background_image, name='background', objects=objects)
 
     # Transformer element
     transform_objects = [
         flip.transformers.data_augmentation.Rotate(mode='random'),
-        flip.transformers.data_augmentation.Flip(mode='y'),
+        flip.transformers.data_augmentation.Flip(mode='x'),
         flip.transformers.data_augmentation.RandomResize(
             mode='symmetric_w',
             relation='parent',
@@ -128,26 +135,29 @@ def create_element(objects_paths, backgrounds_paths):
             w_percentage_max=0.5
         )
     ]
+    
+    
+    transform_backgrounds = [
+        flip.transformers.data_augmentation.Flip('x'),
+        flip.transformers.data_augmentation.Color('xyz')
+    ]
+    
+    
 
     name = uuid.uuid4()
     transform = flip.transformers.Compose(
         [
             flip.transformers.ApplyToObjects(transform_objects),
-            # ts.daug.RotateElement(ts.const.Rotation.ninety),
-            # flip.transformers.data_augmentation.ObjectsRandomResize(
-            #     mode='symmetric_w', w_min=0.25, w_max=0.4, h_min=0.1, h_max=0.4
-            # ), TODO: Modify to work with Apply to Objects
+            flip.transformers.ApplyToBackground(transform_backgrounds),
             flip.transformers.domain_randomization.ObjectsRandomPosition(
-                x_min=0, y_min=0.4, x_max=0.7, y_max=0.7, mode='percentage'
+                x_min=0, y_min=0, x_max=1, y_max=1, mode='percentage'
             ),
-            flip.transformers.data_augmentation.Flip('x'),
-            # ts.ObjectsGetBGColor(), TODO: Modify to work with Apply to Objects
             flip.transformers.domain_randomization.Draw(),
             flip.transformers.labeler.CreateBoundingBoxes(),
+            flip.transformers.labeler.CreateMasks(classes_names), 
             flip.transformers.io.SaveImage(OUT_DIR, name),
-            # flip.transformers.io.CreateCSV(OUT_DIR, name)
-            flip.transformers.io.CreatePascalVoc(OUT_DIR, name),
-        ]
+            flip.transformers.io.SaveMask(OUT_DIR, name)
+       ]
     )
 
     [el] = transform(el)
@@ -196,7 +206,6 @@ def create_google_csv(elements):
                 y2 = 1
 
             csv_data += f"{'TRAIN' if count <= train else ('VALIDATE' if count <= train + validate else 'TEST')},PATH#{element.name}.jpg,{tag['name']},{round(x1, 2)},{round(y1, 2)},,,{round(x2, 2)},{round(y2, 2)},,\n"
-    # print(csv_data)
 
     with open("examples/result/google_data.csv", mode="w") as f:
         f.write(csv_data)
@@ -204,9 +213,4 @@ def create_google_csv(elements):
 
 # ==============================================================================
 create_out_dir()
-setup_environment(OBJECTS_PATTERN, BACKGROUNDS_PATTERN, N_SAMPLES)
-
-# stage = pr.map(create_element, range(N_SAMPLES), workers=WORKERS, on_start=on_start)
-# stage = (x for x in tqdm(stage, total=N_SAMPLES))
-
-# pr.run(stage)
+setup_environment(OBJECTS_PATTERN, BACKGROUNDS_PATTERN, N_SAMPLES, CLASSES_PATTERN)
